386cb4b18c0c6ec0679c1fcf067ae93765dbf46c
Pranav R <pranavrameshk@gmail.com>
2021-01-01 18:29:04 +0000

    Added rpi code

---
+++ src/rpi/motor.js
@@ -0,0 +1,17 @@
+var Gpio = require('onoff').Gpio;
+var motor = new Gpio(4, 'out');
+motor.writeSync(1);
+
+function turnOnMotor() {
+    if (motor.readSync() === 1) {
+        motor.writeSync(0);
+    }
+}
+
+function turnOffMotor() {
+    if (motor.readSync() === 0) {
+        motor.writeSync(1);
+    }
+}
+
+module.exports = {turnOnMotor, turnOffMotor};
