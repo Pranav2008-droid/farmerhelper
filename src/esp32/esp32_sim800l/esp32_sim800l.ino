4c6b7f05dce410fb858ed74d00fd5311cd333645
Pranav R <pranavrameshk@gmail.com>
2021-07-25 20:12:23 +0530

    added the base code for the esp32

---
+++ src/esp32/esp32_sim800l/esp32_sim800l.ino
@@ -0,0 +1,157 @@
+//Select your modem
+//SSL/TLS is currently supported only with SIM8xx series
+#define TINY_GSM_MODEM_SIM800
+
+//Increase RX buffer
+#define TINY_GSM_RX_BUFFER 256
+
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+#include <TinyGsmClient.h> //https://github.com/vshymanskyy/TinyGSM
+#include <ArduinoHttpClient.h> //https://github.com/arduino-libraries/ArduinoHttpClient
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+const char FIREBASE_HOST[]  = "ENTER_FIREBASE_HOST";
+const String FIREBASE_AUTH  = "ENTER_FIREBASE_AUTH";
+const String FIREBASE_PATH  = "/";
+const int SSL_PORT          = 443;
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+// Your GPRS credentials
+// Leave empty, if missing user or pass
+char apn[]  = "airtelgprs.com";
+char user[] = "";
+char pass[] = "";
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//GSM Module RX pin to ESP32 2
+//GSM Module TX pin to ESP32 4
+#define rxPin 4
+#define txPin 2
+HardwareSerial sim800(1);
+#define TINY_GSM_DEBUG Serial
+#define DUMP_AT_COMMANDS
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(sim800, Serial);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(sim800);
+#endif
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+TinyGsmClientSecure gsm_client_secure_modem(modem, 0);
+HttpClient http_client = HttpClient(gsm_client_secure_modem, FIREBASE_HOST, SSL_PORT);
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
+unsigned long previousMilli;
+long interval = 10000;
+
+//**************************************************************************************************
+void setup() {
+  Serial.begin(115200);
+  Serial.println("esp32 serial initialize");
+  
+  sim800.begin(115200, SERIAL_8N1, rxPin, txPin);
+  Serial.println("SIM800L serial initialize");
+
+
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //Restart takes quite some time
+  //To skip it, call init() instead of restart()
+  String modemInfo = modem.getModemInfo();
+  Serial.print("Modem1: ");
+  Serial.println(modemInfo);
+
+  Serial.println("Initializing modem...");
+  modem.restart();
+  modemInfo = modem.getModemInfo();
+  Serial.print("Modem2: ");
+  Serial.println(modemInfo);
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  
+  // Unlock your SIM card with a PIN
+  modem.simUnlock("0000");
+  
+  http_client.setHttpResponseTimeout(90 * 1000); //^0 secs timeout
+  Serial.print("Setup completed\n");
+}
+//**************************************************************************************************
+
+
+//**************************************************************************************************
+void loop() {
+  Serial.print("Connecting to ");
+  Serial.print(apn);
+  if (!modem.gprsConnect(apn, user, pass)) {
+    Serial.println(" fail");
+    delay(1000);
+    return;
+  }
+  Serial.println("GPRS connection successful");
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  
+  http_client.connect("www.google.com", SSL_PORT);
+  
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  while (true) {
+    if (!http_client.connected()) {
+      Serial.println();
+      http_client.stop();// Shutdown
+      Serial.println("HTTP  not connect");
+      break;
+    }
+    else{
+      Serial.println("\nConnected to google.com");
+      //PostToFirebase("PATCH", FIREBASE_PATH, "hello world", &http_client);
+    }
+  }
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+}
+//**************************************************************************************************
+
+//**************************************************************************************************
+void PostToFirebase(const char* method, const String & path , const String & data, HttpClient* http) {
+  String response;
+  int statusCode = 0;
+  http->connectionKeepAlive(); // Currently, this is needed for HTTPS
+  
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  String url;
+  if (path[0] != '/') {
+    url = "/";
+  }
+  url += path + ".json";
+  url += "?auth=" + FIREBASE_AUTH;
+  Serial.print("POST:");
+  Serial.println(url);
+  Serial.print("Data:");
+  Serial.println(data);
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  
+  String contentType = "application/json";
+  http->put(url, contentType, data);
+  
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  // read the status code and body of the response
+  //statusCode-200 (OK) | statusCode -3 (TimeOut)
+  statusCode = http->responseStatusCode();
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  response = http->responseBody();
+  Serial.print("Response: ");
+  Serial.println(response);
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  if (!http->connected()) {
+    Serial.println();
+    http->stop();// Shutdown
+    Serial.println("HTTP POST disconnected");
+  }
+  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+}
+//**************************************************************************************************
